78064147f25fba0199ef556cde05921055236300
Walid-Belkassmi <belkassmi.walid@gmail.com>
2022-07-10 22:04:31 +0200

    try filter function

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const toDoContainer = document.getElementById("to-do-container") //Partie dans l
 const taskInput = document.getElementById("input-text") //Barre de saisie des tâches
 const checkboxArea = document.getElementById('checkbox-1')
 const valueButon = document.querySelectorAll(".value-btn")
-const editButton = document.getElementById("btn-pencil")
 const dice = document.getElementById("btn-dice") // Récupération du Dé qui génère aléatoirement une task
+const doingButton = document.getElementById("doing") // Récupération du boutton To Do
 
 
 
 
 let taskWrapper = []
 let y = ""
+
 
 const submitText = submitButton.onclick = (e) => {
 
@@ -43,8 +44,8 @@ const submitText = submitButton.onclick = (e) => {
 
             display += `
             <div class="status-buttons">
-              <button type="button" class="btn-todo-2" id="btn-todo">to do</button>
-              <button type="button" class="btn-doing-2" id="btn-doing">doing</button>
+              <button type="button" class="btn-todo-2" id="btn-todo" onclick="todoStatus()">to do</button>
+              <button type="button" class="btn-doing-2" id="btn-doing" onclick="doingStatus()">doing</button>
             </div> 
 
             <div class="task" id="${task.id}">
@@ -71,6 +72,50 @@ const submitText = submitButton.onclick = (e) => {
     }
 
 }
+
+const todoStatus = () =>{
+    taskWrapper.status= "To Do"
+}
+
+const doingStatus = () => {
+    taskWrapper.status = "Doing"
+}
+
+
+doingButton.addEventListener("click", () => {
+    taskWrapper.filter((task) => {
+        if(task.status === "Doing"){
+
+            let display = ""
+            taskWrapper.forEach((task) => {
+            display += `
+                <div class="status-buttons">
+                <button type="button" class="btn-todo-2" id="btn-todo" onclick="todoStatus()">to do</button>
+                <button type="button" class="btn-doing-2" id="btn-doing" onclick="doingStatus()">doing</button>
+                </div> 
+
+                <div class="task" id="${task.id}">
+                    <input type="checkbox" class="checkbox" id="checkbox${task.id}">
+                    <div class="entered-text">
+                        <p id="main-task">${task.name}</p><span class="span">${task.value}</span>
+                        
+                        <button class="btn-pencil" id="btn-pencil" onClick="editText()">
+                            <i class="fa-solid fa-pencil icon-pencil"></i>
+                        </button>
+
+                        <button class="btn-x" onclick="removeTask(${task.id})"><i
+                                class="fa-solid fa-xmark  icon-x"></i></button>
+                    </div>
+                </div>
+                `
+            y = ""
+
+        })
+
+        document.querySelector("#list").innerHTML = display
+        }
+    })
+})
 
 
 const editText = () => {
